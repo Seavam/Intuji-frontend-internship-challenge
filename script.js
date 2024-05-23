2bcd387415c74e5dec3758c388f210d0544536d1
Seavam <rautshivam9@gmail.com>
2024-05-23 23:34:35 +0545

    upadate:"completed_intuji_frontend_challenge"

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ if (close){
         nav.classList.remove('active');
     })
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const headers = document.querySelectorAll(".accordion-header");
+  
+    headers.forEach(header => {
+      header.addEventListener("click", function() {
+        const content = this.nextElementSibling;
+  
+        // Collapse other open sections
+        document.querySelectorAll('.accordion-content').forEach(item => {
+          if (item !== content) {
+            item.style.maxHeight = null;
+          }
+        });
+  
+        // Toggle the current section
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+        }
+      });
+    });
+  });
+  
